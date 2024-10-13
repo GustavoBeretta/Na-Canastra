b81927c14a6aca12d20ee9a48a32178df3e48dd6
Rafaela Borges <rafasborgesw@gmail.com>
2024-10-13 17:05:47 -0300

    feat: controllers

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,14 @@
+import Product from "../models/product.js";
+
+async function getProduct(request, response) {
+    const product = await Product.find();
+    return response.status(200).json(product);
+}
+
+async function createProduct(request, response) {
+    const product = request.body;
+    const newProduct = await Product.create(product);
+    return response.json(newProduct);
+}
+
+export { createProduct, getProduct };
