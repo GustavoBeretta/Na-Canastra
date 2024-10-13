e388e9b06692ee8ce9bbd1e57ff000369a5416ba
Rafaela Borges <rafasborgesw@gmail.com>
2024-10-13 10:30:01 -0300

    feat: melhorando a organização

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express, { request, response } from "express";
+import User from "./models/user.js";
+import connectDatabase from "./database/db.js";
+import product from "./models/product.js";
+const app = express();
+
+app.use(express, json());
+app.get("./users", async (request, response) => {
+  const users = await User.find();
+  return response.status(200).json(users);
+});
+
+app.post("./users", async (request, response) => {
+  const user = request.body;
+  const newUser = await User.create(user);
+  return response.json(newUser);
+});
+
+connectDatabase()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Servidor rodando e banco de dados conectados");
+    });
+  })
+  .catch(() => console.log("Deu ruim"));
