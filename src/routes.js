d79472169f48e618e594f9714f48d34a18952066
Rafaela Borges <rafasborgesw@gmail.com>
2024-10-13 17:50:34 -0300

    fix: arrumando rotas

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { getUsers, createUsers } from "./controllers/UserController.js";
-import { createProduct, getProducts } from "./controllers/ProductController.js";
+import { createProduct, getProducts, updateProduct, deleteProduct } from "./controllers/ProductController.js";
 const routes = Router();
 
 routes.get("/Users", getUsers);
 routes.post("/Users", createUsers);
 routes.get("/Product", getProducts);
 routes.post("/Product", createProduct);
-router.put('/products/:id', updateProduct); // Rota para atualizar o produto
-router.delete('/products/:id', deleteProduct);
+routes.put('/products/:id', updateProduct); // Rota para atualizar o produto
+routes.delete('/products/:id', deleteProduct);
 
 export default routes;
