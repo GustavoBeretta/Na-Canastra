094d66faf0d864fd04c944d56df4b61dfaedef9c
Rafaela Borges <rafasborgesw@gmail.com>
2024-10-20 17:23:56 -0300

    feat: adicionado firebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import express, { request, response } from "express";
+import express from "express";
 import connectDatabase from "./database/db.js";
 import routes from "./routes.js";
+import { initializeApp } from "firebase/app";
+
 const app = express();
+
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyB5xHxX2lOfexhgouSNNK2jihDAvEHUUtI",
+    authDomain: "nacanastra-10dc3.firebaseapp.com",
+    projectId: "nacanastra-10dc3",
+    storageBucket: "nacanastra-10dc3.appspot.com",
+    messagingSenderId: "107671828494",
+    appId: "1:107671828494:web:836f59ffad206e263ee45f",
+    measurementId: "G-3WLT7EFKCG"
+};
+
+// Inicializar Firebase
+initializeApp(firebaseConfig);
 
 app.use(express.json());
 app.use(routes);
+
+// Conectar ao banco de dados MongoDB
 connectDatabase()
     .then(() => {
-    app.listen(3000, () => {
-    console.log("Servidor rodando e banco de dados conectados");
-    });
-})
-.catch(() => console.log("Deu ruim"));
+        app.listen(3000, () => {
+            console.log("Servidor rodando e banco de dados conectado");
+        });
+    })
+    .catch(() => console.log("Deu ruim"));
--- src/models/product.js
+++ src/models/product.js
@@ -4,20 +4,23 @@ const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-},
+    },
     preco: {
         type: Number,
         required: true,
-},
+    },
     peso: {
         type: Number,
         required: true,
-},
+    },
     tipo: {
         type: String,
         enum: ['Queijos', 'Búfula', 'Zero Lactose', 'Doces', 'Variedades'],
         required: true,
-},
+    },
+    imageURL: {
+        type: String
+    }
 });
 
 export default mongoose.model("Product", productSchema);
