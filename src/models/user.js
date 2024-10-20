3ca7c2180ee62d0e47be46bc44077e3d24c17861
Rafaela Borges <rafasborgesw@gmail.com>
2024-10-20 17:37:55 -0300

    feat: adicionando schema usuario

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,27 @@
 import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    cep: {
+        type: String,
+        required: true,
+    },
+    endereco: {
+        type: String,
+        required: true,
+    },
+    cpf: {
+        type: String,
+        required: true,
+        unique: true, // Para garantir que não existam CPFs duplicados
+    },
+    telefone: {
+        type: String,
+        required: true,
+    }
 });
 
 export default mongoose.model("User", userSchema);
